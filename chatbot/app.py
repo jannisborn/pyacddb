13510e6110975b61de395c7c7c3cc27d3e513ce3
jannisborn <jannis.born@gmx.de>
2024-04-18 21:09:34 +0200

    refactor: secret handling

--- chatbot/app.py
+++ chatbot/app.py
@@ -8,18 +8,12 @@ def main():
     """Start the bot."""
     with open(os.path.join(os.path.dirname(__file__), "secrets.json"), "r") as f:
         secrets = json.load(f)
-    telegram_token = secrets["telegram-token"]
-    anyscale_token = secrets["anyscale"]
 
     metadata_path = "db.csv"
     storage_path = "imgs"
+    storage_path = secrets["smartdrive-host"]
 
-    bot = ACDReceive(
-        metadata_path,
-        storage_path,
-        telegram_token=telegram_token,
-        anyscale_token=anyscale_token,
-    )
+    bot = ACDReceive(metadata_path, storage_path, secrets=secrets)
     bot.run()
 
 
