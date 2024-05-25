3bab48454d4ed656e1490e839f430af004fc37c0
Jannis Born <jannis.born@gmx.de>
2024-05-25 20:03:12 +0200

    bugfixes and dispaly randomization (#9)

--- chatbot/acdreceive/dataclient.py
+++ chatbot/acdreceive/dataclient.py
@@ -21,7 +21,6 @@ class Client:
         response = requests.get(url, auth=HTTPBasicAuth(self.username, self.password))
 
         if response.status_code == 200:
-            logger.debug(f"Loaded file {url}")
             return response.content
         else:
             logger.error(
